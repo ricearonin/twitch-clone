4f120afe5b7096a646bbcbfdd3d03419af591a7c
Taylor <taylor.a.rice@gmail.com>
2019-08-04 21:35:36 -0700

    scaffolding for components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom"; //Make sure you use react-router-dom here.
+
+import Header from "./Header";
+
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+
+//Base component
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
